ce62f0e5e451048c22c6ba0dddecd096f0a5c810
KatouProg <katouprog@gmail.com>
2022-01-24 18:38:02 +0900

    FRONT - Dev page d'accueil

--- frontend/assets/js/vue.js
+++ frontend/assets/js/vue.js
@@ -1,8 +1,24 @@
+// DB provisoire
 
+const partages = [
+    { id: 1, description: "Looool", img: 'assets/img/Hero-Elf4IhEXUAApPJQ.jpeg'},
+    { id: 2, description: 'Mdrrr', img: 'assets/img/Hero-Elf4IhEXUAApPJQ.jpeg'},
+    { id: 3, description: 'Trop drole wallah !!!', img: 'assets/img/Hero-Elf4IhEXUAApPJQ.jpeg'},
+    { id: 4, description: 'Ahahahah', img: 'assets/img/Hero-Elf4IhEXUAApPJQ.jpeg'},
+    { id: 5, description: 'Ohohohoh', img: 'assets/img/Hero-Elf4IhEXUAApPJQ.jpeg'},
+    { id: 6, description: 'Xptdrrrrrr', img: 'assets/img/Hero-Elf4IhEXUAApPJQ.jpeg'},
+    { id: 7, description: 'Ptdrrrr', img: 'assets/img/Hero-Elf4IhEXUAApPJQ.jpeg'},
+    { id: 8, description: 'Pfffffffff...', img: 'assets/img/Hero-Elf4IhEXUAApPJQ.jpeg'},
+  ];
 
 const Accueil = {
-    template: '<h1>Accueil</h1>',
-    name: 'Accueil'
+    template: "#accueil",
+    name: 'Accueil',
+    data: () => {
+        return {
+            partages,
+        }
+    }
 }
 const Signup= {
     template: '<h1>Sign up</h1>',
